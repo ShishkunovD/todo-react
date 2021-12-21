16198bd7bf30fdfc5022b06433995f453b4bbb12
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-21 17:06:29 +0300

    added delete task

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
+import basket from './basket.svg';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -23,6 +24,11 @@ const App = () => {
     allTask[id].isCheck = !allTask[id].isCheck;
     setAllTask([...allTask]);
   }
+
+  const deleteTask = (index) => {
+    allTask.splice(index, 1);
+    setAllTask([...allTask]);
+  }
   
   return (
     <div className="container">
@@ -30,9 +36,10 @@ const App = () => {
         <input className="input-task" onChange={(e) => changeInput(e)} value={input} />
         <button className="btn-task" onClick={() => addButton()}>Добавить</button>
       </div>
-      {allTask.map((item, index) => <div key={index}>
+      {allTask.map((item, index) => <div key={index} className="task-container">
         <input type="checkbox" defaultChecked={item.isCheck} onChange={() => changeCheckbox(index)}/>
         <span className={item.isCheck ? 'throught': ''}>{item.task}</span>
+        <span ><img src={basket} className="basket" alt="basket" onClick={() => deleteTask(index)}/></span>
       </div>)
       }
     </div>
