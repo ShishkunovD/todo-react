43abdd11a968f0af97c65432d4a7cee1b177b94e
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-21 15:01:20 +0300

    fix bugs after the third comments

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import './App.css';
 import React, { useState } from 'react';
-import Tasks from './Tasks';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [allTask, setAllTask] = useState([]);
 
-  const changeInput = (event) => {
-    setInput(event.target.value);
+  const changeInput = (e) => {
+    setInput(e.target.value);
   }
 
   const addButton = () => {
-    let id = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2);
     const newTask = {
-      ident: id,
       task: input,
       isCheck: false
-    };  
+    };
     allTask.push(newTask);
     setAllTask([...allTask]);
     setInput('');
   }
 
+  const changeCheckbox = (id) => {
+    allTask[id].isCheck = !allTask[id].isCheck;
+    setAllTask([...allTask]);
+  }
+  
   return (
     <div className="container">
       <div className="add-task">
         <input className="input-task" onChange={(e) => changeInput(e)} value={input} />
         <button className="btn-task" onClick={() => addButton()}>Добавить</button>
       </div>
-      {allTask.map(item => <Tasks key={`Task-${item.ident}`} task={item.task} isCheck={item.isCheck}/>)}
+      {allTask.map((item, index) => <div key={index}>
+        <input type="checkbox" defaultChecked={item.isCheck} onChange={() => changeCheckbox(index)}/>
+        <span className={item.isCheck ? 'throught': ''}>{item.task}</span>
+      </div>)
+      }
     </div>
   );
 }
