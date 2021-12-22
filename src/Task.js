70f75e54842379d4b4ebd55d4707acb5208ded52
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-22 18:02:11 +0300

    fix bugs after the second comments

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,10 @@
-import './App.css';
 import React, { useState } from 'react';
 import basket from './basket.svg';
 import pancel from './pancel.png';
 
 const Task = ({item, changeCheckbox, index, deleteTask, saveEditing}) => {
   const { task, isCheck } = item;
-  const [changeState, setChangeState] = useState(!!0);
+  const [changeState, setChangeState] = useState(false);
   const [editInput, setEditInput] = useState(task);
 
   const changeInput = (e) => {
@@ -27,26 +26,41 @@ const Task = ({item, changeCheckbox, index, deleteTask, saveEditing}) => {
   
   return (
     <div className="task-container">
-      {changeState &&  <div className="edit-conatiner" >
-        <button onClick={() => cancelEditing()}>Отмена</button>
-        <input onChange={(e) => changeInput(e)} value={editInput} className="inputEditing"/>
-        <button onClick={() => {saveEditing(editInput, index); renderSave()}}>Сохранить</button>
-      </div>}
-      {!changeState && <div className="main-container">
-        <div className="left-block">
-          <input type="checkbox" checked={isCheck} onChange={() => changeCheckbox(index)}/>
-          <span className={isCheck ? 'throught task': 'task'}>{task}</span>
-        </div>
-        <span><img src={pancel} className="pancel" alt="pancel" onClick={() => editTask(index)} /></span>
-      </div>}
+      <div>
+        {changeState && 
+        <div className="edit-conatiner" >
+          <button onClick={() => cancelEditing()}>Отмена</button>
+          <input onChange={(e) => changeInput(e)} value={editInput} className="inputEditing"/>
+          <button onClick={() => {saveEditing(editInput, index); renderSave()}}>Сохранить</button>
+        </div>}
+      </div>
+
+      <div className="wrapper">
+        {!changeState && 
+        <div className="main-container">
+          <div className="left-block">
+            <input type="checkbox" checked={isCheck} onChange={() => changeCheckbox(index)}/>
+            <span className={isCheck ? 'throught task': 'task'}>{task}</span>
+          </div>
+          <span>
+            <img 
+              src={pancel}
+              className="pancel"
+              alt="pancel"
+              onClick={() => editTask(index)} 
+            />
+          </span>
+        </div>}
+      </div>
+
       <span>
         <img
-        src={basket}
-        className="basket"
-        alt="basket"
-        onClick={ () => deleteTask(index) }
+          src={basket}
+          className="basket"
+          alt="basket"
+          onClick={ () => deleteTask(index) }
         />
-        </span>
+      </span>
     </div>
   );
 }
