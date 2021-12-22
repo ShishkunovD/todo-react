57235c1ebb7944fbac6321a6913227ae9a580c03
Dmitriy <dshishkunov.exceedteam@gmail.com>
2021-12-22 12:53:11 +0300

    added editing for our tasks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import basket from './basket.svg';
+import pancel from './pancel.png';
 
 const App = () => {
   const [input, setInput] = useState('');
@@ -29,6 +30,56 @@ const App = () => {
     allTask.splice(index, 1);
     setAllTask([...allTask]);
   }
+
+  const editTask = (index) => {
+    const mainContainers = document.querySelectorAll('.main-container');
+    const editContainers = document.querySelectorAll('.edit-container');
+    for(let i = 0; i < mainContainers.length; i++) {
+      if(i === index) {
+        mainContainers[i].classList.add('hide');
+      };
+    }
+    for(let k = 0; k < editContainers.length; k++) {
+      if(k === index) {
+        editContainers[k].classList.remove('hide');
+      }
+    }
+  }
+
+  const cancelEditing = (index) => {
+    const mainContainers = document.querySelectorAll('.main-container');
+    const editContainers = document.querySelectorAll('.edit-container');
+    for(let i = 0; i < mainContainers.length; i++) {
+      if(i === index) {
+        mainContainers[i].classList.remove('hide');
+      };
+    }
+    for(let k = 0; k < editContainers.length; k++) {
+      if(k === index) {
+        editContainers[k].classList.add('hide');
+      }
+    }
+  }
+
+  const saveEditing = (id) => {
+    const inputEditings = document.querySelectorAll('.inputEditing');
+    const tasks = document.querySelectorAll('.task');
+    let inputEdit = inputEditings[0];
+    inputEditings.forEach((item, index) => {
+      if(index === id) {
+        inputEdit = item;
+      }
+    });
+    let task = tasks[0];
+    tasks.forEach((item, index) => {
+      if(index === id) {
+        task = item;
+      }
+    });
+    task.innerText = inputEdit.value;
+    setAllTask([...allTask]);
+    cancelEditing(id);
+  }
   
   return (
     <div className="container">
@@ -37,9 +88,26 @@ const App = () => {
         <button className="btn-task" onClick={() => addButton()}>Добавить</button>
       </div>
       {allTask.map((item, index) => <div key={index} className="task-container">
-        <input type="checkbox" defaultChecked={item.isCheck} onChange={() => changeCheckbox(index)}/>
-        <span className={item.isCheck ? 'throught': ''}>{item.task}</span>
-        <span ><img src={basket} className="basket" alt="basket" onClick={() => deleteTask(index)}/></span>
+      <div className="edit-container hide">
+        <button onClick={() => cancelEditing(index)}>Отмена</button>
+        <input defaultValue={item.task} className="inputEditing"/>
+        <button onClick={() => saveEditing(index)}>Сохранить</button>
+      </div>
+      <div className="main-container">
+        <div className="left-block">
+          <input type="checkbox"  defaultChecked={item.isCheck} onChange={() => changeCheckbox(index)}/>
+          <span className={item.isCheck ? 'throught task': 'task'}>{item.task}</span>
+        </div>
+        <span><img src={pancel} className="pancel" alt="pancel" onClick={() => editTask(index)}/></span>
+      </div>
+      <span>
+        <img
+        src={basket}
+        className="basket"
+        alt="basket"
+        onClick={ () => deleteTask(index) }
+        />
+        </span>
       </div>)
       }
     </div>
